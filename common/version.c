09fa24695de06b3531833a4c1205c33936b4650f
cloudesteem <rd@cloudesteem.cn>
2024-09-29 09:37:46 +0000

    update yocto 6.6.36 2.1.0 uboot V1.0.2

--- common/version.c
+++ common/version.c
@@ -10,7 +10,7 @@
 
 //John_gao add uboot version
 #if 1
-#define Debix_C_VERSION " Debix Model AB V1.0.1 "
+#define Debix_C_VERSION " Debix_Model_AB V1.0.2 "
 #define U_BOOT_VERSION_STRING Debix_C_VERSION " (" U_BOOT_DATE " - " U_BOOT_TIME ")"
 
 #else
