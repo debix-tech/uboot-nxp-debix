8f31f43e45193dce43ebe83bea09311097090c82
cloudesteem <rd@cloudesteem.cn>
2024-11-14 07:33:09 +0000

    EMB-IMX8MP-07 V1.0.1 add EMB-IMX8MP-07 board

--- include/configs/imx8mp_evk.h
+++ include/configs/imx8mp_evk.h
@@ -92,6 +92,7 @@
 	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"bsp_script=boot.scr\0" \
 	"image=Image\0" \
+	"splashpos=m,m\0"			\
 	"debix_version=" CONFIG_Debix_VERSION "\0"                      \
 	"splashimage=0x50000000\0" \
 	"console=ttymxc1,115200\0" \
